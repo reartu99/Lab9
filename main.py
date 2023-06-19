8651030d3778bc2224913197c8ec4a46d98cc3d4
Arturo <arturo.masullo2003@icloud.com>
2023-06-20 01:40:49 +0200

    Commit

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ def media_varianza_min_max(arr):
     print("Il massimo ed il minimo valore registrati sono:", max(arr), min(arr))
     print("")
     print("")
+    return np.mean(arr)
 
 
 def histog(arr, orient, nome):
-    nbins = int(np.ceil(np.log2(len(arr)))) + 1   # Scegliamo il numero di bins con la regola di Stourges
+    nbins = int(np.ceil(np.log2(len(arr)))) + 1  # Scegliamo il numero di bins con la regola di Stourges
 
     if orient == 1:
         colors = 'red'
@@ -24,22 +25,48 @@ def histog(arr, orient, nome):
     plt.title("Distanza " + nome[5:7:] + " cm " + "configurazione " + nome[11:12:])
     plt.show()
     print("Distanza (cm): " + nome[5:7:])
-    media_varianza_min_max(arr)
 
 
-def apri(nome, orient):
+def apri(nome, orient, medie):
     df = pd.read_csv(nome, on_bad_lines='skip')
     colonna = df['Ultimo']
     histog(colonna, orient, nome)
+    value = media_varianza_min_max(colonna)
+    medie.append(value)
     return colonna
 
-global listmedie
 
-apri("venv/35conf1.csv", 1)
-apri("venv/35conf2.csv", 2)
-apri("venv/36conf1.csv", 1)
-apri("venv/36conf2.csv", 2)
-apri("venv/37conf1.csv", 1)
-apri("venv/37conf2.csv", 2)
-apri("venv/39conf1.csv", 1)
-apri("venv/39conf2.csv", 2)
+def tstarcalc(T11, T12, T22, T21, et):
+    tstar = ((T11*T22)-(T12*T21))/((T22*T21)-(T12*T11))
+    errtstar = 0
+    return tstar, errtstar
+
+
+def linee(ranges, T11, T12, T22, T21, x1, x2):
+    linearossa = []
+    lineablue = []
+    for x in ranges:
+        linearossa.append(((T21-T11)/(x2-x1))*(x-x1)+T11)
+    for x in ranges:
+        lineablue.append(((T22-T12)/(x2-x1))*(x-x1)+T12)
+    return linearossa, lineablue
+
+meds = []
+intervalli = [35, 36, 37, 39]
+
+apri("venv/35conf1.csv", 1, meds)
+apri("venv/35conf2.csv", 2, meds)
+apri("venv/36conf1.csv", 1, meds)
+apri("venv/36conf2.csv", 2, meds)
+apri("venv/37conf1.csv", 1, meds)
+apri("venv/37conf2.csv", 2, meds)
+apri("venv/39conf1.csv", 1, meds)
+apri("venv/39conf2.csv", 2, meds)
+
+plt.errorbar(intervalli, meds[::2], xerr=0.1, fmt='o')
+plt.errorbar(intervalli, meds[1::2], xerr=0.1, fmt='o', color='red')
+lrossa, lblue = linee(intervalli, meds[0], meds[1], meds[3], meds[4], 35, 36)
+plt.plot(lrossa)
+plt.plot(lblue)
+plt.show()
+print("T* sarebbe:" + str(tstarcalc(meds[0], meds[1], meds[3], meds[4], 1)))
