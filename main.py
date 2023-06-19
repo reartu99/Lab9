abcd67896481b379ba9a0268bbbee0096a950a67
Arturo <arturo.masullo2003@icloud.com>
2023-06-19 22:52:46 +0200

    Commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -9,20 +8,38 @@ def media_varianza_min_max(arr):
     print("La varianza è:", np.var(arr))
     print("La stdev è:", np.std(arr))
     print("Il massimo ed il minimo valore registrati sono:", max(arr), min(arr))
+    print("")
+    print("")
 
 
-def histog(arr):
-    nbins = np.log2(len(arr)) + 1   # Scegliamo il numero di bins con la regola di Stourges
-    plt.hist(arr, bins=nbins, color='green')
+def histog(arr, orient, nome):
+    nbins = int(np.ceil(np.log2(len(arr)))) + 1   # Scegliamo il numero di bins con la regola di Stourges
+
+    if orient == 1:
+        colors = 'red'
+    else:
+        colors = 'blue'
+
+    plt.hist(arr, bins=nbins, color=colors)
+    plt.title("Distanza " + nome[5:7:] + " cm " + "configurazione " + nome[11:12:])
+    plt.show()
+    print("Distanza (cm): " + nome[5:7:])
     media_varianza_min_max(arr)
 
 
-def apri(nome):
+def apri(nome, orient):
     df = pd.read_csv(nome, on_bad_lines='skip')
     colonna = df['Ultimo']
-    print(colonna)
+    histog(colonna, orient, nome)
     return colonna
 
+global listmedie
 
-columns = apri("venv/35 configurazione 1.csv")
-media_varianza_min_max(columns)
+apri("venv/35conf1.csv", 1)
+apri("venv/35conf2.csv", 2)
+apri("venv/36conf1.csv", 1)
+apri("venv/36conf2.csv", 2)
+apri("venv/37conf1.csv", 1)
+apri("venv/37conf2.csv", 2)
+apri("venv/39conf1.csv", 1)
+apri("venv/39conf2.csv", 2)
